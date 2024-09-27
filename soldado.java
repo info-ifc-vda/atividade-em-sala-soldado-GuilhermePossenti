4006150738049045f66fcff790c5eab499d3b6c1
GuilhermePossenti <gui.possenti789@gmail.com>
2024-09-27 08:38:51 -0300

    Add files via upload
    Táticas de movimentação de um soldado

---
+++ soldado.java
@@ -0,0 +1,66 @@
+package batalhao;
+
+public class soldado {
+    private String nome;
+    private int posX;
+    private int posY;
+
+    public soldado(String nome) {
+        this.nome = nome;
+        this.posX = 0;
+        this.posY = 0;
+    }
+
+    public void moverParaCima() {
+        setPosY(getPosY() + 1); 
+
+    public void moverParaBaixo() {
+        setPosY(getPosY() - 1); 
+    }
+
+    public void moverParaDireita() {
+        setPosX(getPosX() + 1); 
+    }
+
+    public void moverParaEsquerda() {
+        setPosX(getPosX() - 1); 
+    }
+
+    public void exibirPosicao() {
+        System.out.println("Posição do soldado: (" + getPosX() + ", " + getPosY() + ")");
+    }
+
+    public void atacar(Inimigo inimigo, int dano) { 
+        if (inimigo != null) {
+            inimigo.receiveDamage(dano);
+            System.out.println("You attacked the enemy for " + dano + " damage!");
+        } else {
+            System.out.println("No enemy to attack!");
+        }
+    }
+
+    // Gets e sets
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+}
